9106c113fd2cf1cc026e0d7eb8b018e213aa463a
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2023-12-12 16:38:29 +0200

    add error-handling level

---
+++ app/error/filter/dal.ts
@@ -0,0 +1,18 @@
+export default ({ logger, lib }) =>
+    (error: Error) => {
+        const codes = {
+            '23505': lib.exeption.dal.AlreadyExists,
+            '23503': lib.exeption.dal.ForeignKeyError,
+            '42P01': lib.exeption.dal.MissingTableError,
+            '42703': lib.exeption.dal.MissingColumnError,
+            '42601': lib.exeption.dal.SyntaxError,
+        };
+        const { code } = error as any;
+        if (code in codes) {
+            logger.error(error);
+            const Exeption = codes[code];
+            console.log(Exeption);
+            throw new Exeption();
+        }
+        throw error;
+    };
