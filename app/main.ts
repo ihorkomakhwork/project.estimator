208247a8df145411e98c1738064c37ea74dce637
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2023-12-12 16:39:44 +0200

    add ajv schema lib

--- app/main.ts
+++ app/main.ts
@@ -1,14 +1,18 @@
 import app from './application';
 import lib from '../lib/lib';
 
+app.logger.info('Starting application...');
+
 new lib.Server(app);
 
 process.on('uncaughtException', (err) => {
+    app.logger.info('Gracefully shutting down...');
     app.logger.info('uncaughtException');
     app.logger.error(err);
     process.exit(1);
 });
 process.on('unhandledRejection', (err) => {
+    app.logger.info('Gracefully shutting down...');
     app.logger.info('unhandledRejection');
     app.logger.error(err);
     process.exit(1);
