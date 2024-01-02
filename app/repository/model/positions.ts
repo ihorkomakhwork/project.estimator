4786b8e11ebe84b0e1d3cc6f09158bfcd4623f36
ihorkomakhwork <I.Komakh.FIT.122.20@knute.edu.ua>
2024-01-02 15:30:31 +0200

    add models

---
+++ app/repository/model/positions.ts
@@ -0,0 +1,6 @@
+import type { IPosition } from '../../contract/idomain';
+import IContainer from '../../contract/icrontainer';
+
+export default ({ repository }: IContainer) => ({
+    ...repository.model<IPosition>('positions'),
+});
